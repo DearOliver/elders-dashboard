e771099861a9dbf7cfa7beb6ed7d646aa86dfae2
DearOliver <romain.loubeyre@yahoo.fr>
2024-04-28 20:45:49 +0200

    db

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 
 app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://dispatch_admin:UtNg2LqPtPX4Tt96@localhost/dearo_dispatch'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://ucqnhvk6u5rvsr:p90d1e1fcd767495447f50c9db8447f74bed656c9882c6dc3d1de7cb392ace2ca@cdgn4ufq38ipd0.cluster-czz5s0kz4scl.eu-west-1.rds.amazonaws.com:5432/d50i6d64k7h43f'
 db = SQLAlchemy(app)
 
 class User(db.Model):
