21dcf709b3c44cfbc2e813dfb480268b9c34f572
DearOliver <romain.loubeyre@yahoo.fr>
2024-04-28 21:27:47 +0200

    test

--- app.py
+++ app.py
@@ -6,13 +6,9 @@ import os
 app = Flask(__name__)
 
 app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
-print("TEST")
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://ucqnhvk6u5rvsr:p90d1e1fcd767495447f50c9db8447f74bed656c9882c6dc3d1de7cb392ace2ca@cdgn4ufq38ipd0.cluster-czz5s0kz4scl.eu-west-1.rds.amazonaws.com:5432/d50i6d64k7h43f'
-print("TEST 2")
 db = SQLAlchemy(app)
-print("TEST 3")
 port = int(os.environ.get("PORT", 5000))
-print("TEST 4")
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -36,10 +32,6 @@ class Intervention(db.Model):
 @app.route("/")
 def index():
     return render_template("index.html")
-
-@app.route("/test")
-def test():
-    return render_template("test.html")
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
@@ -88,7 +80,9 @@ def create_patrol(user_id):
 
     patrols = Patrol.query.all()
     for patrol in patrols :
+        print("TEST : ", patrol.id)
         if User.query.filter_by(patrol_id=patrol.id).count() == 0 :
+            print("TRUE")
             Patrol.query.filter_by(id=patrol.id).delete()
 
     return redirect(url_for("home"))
